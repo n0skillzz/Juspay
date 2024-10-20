9bd33c75a89a82968376e7127cd844d6392552af
Pramod Katare <126380267+n0skillzz@users.noreply.github.com>
2024-10-20 15:43:21 +0530

    scratch-from-scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { EventBody } from './Components/eventBody'
+import { useState } from 'react';
+import { NavBar } from './Components/navBar';
+import { DragDropContext} from "react-beautiful-dnd";
+import { MOVES } from "./constants";
 
-function App() {
+export default function App() {
+  const [moves, setMoves] = useState(MOVES);
+  const [actions, setActions]= useState([]);
+  const [actions2, setActions2]= useState([]);
+  
+  const onHandleDragEnd = (result) =>{
+    const {source, destination} = result;
+    console.log(source, destination)
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    let add , 
+      active = moves,
+      complete = actions,
+      complete2 = actions2; 
+
+    // take a move to drag and drop 
+    add = active[source.index];
+
+    // Destination Logic
+    if (destination.droppableId === "MovesActions") {
+      // add the move to action 1
+      complete.push(add);
+    } else {
+      //add the move to action 2
+      complete2.push(add);
+    }
+    setActions2(complete2);
+    setActions(complete);
+    setMoves(active);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-100 font-sans text-center">
+      <NavBar/>
+        <DragDropContext onDragEnd={onHandleDragEnd}>
+          <EventBody 
+            moves={moves} 
+            setMoves={setMoves} 
+            actions={actions}
+            actions2={actions2}
+            setActions2={setActions2}
+            setActions={setActions}  
+          />
+        </DragDropContext>
     </div>
   );
 }
-
-export default App;
---
+++ src/Components/navBar.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Logo from '../Assets/images/Logo.png';
+import { PAGES } from '../constants';
+
+export const NavBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{backgroundColor : '#4d97ff'}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+             sx={{
+                mr:5,
+                mt:1,
+                ml:5,
+                display: { xs: 'none', md: 'flex' },
+              }}
+          >
+            <img src={Logo} style={{ height: '50px', display: 'flex'}} />
+          </Box>  
+          <Box
+             sx={{
+                mr: '30%',
+                ml: '35%',
+                display: { xs: 'flex', md: 'none' },
+              }}
+          >
+            <img src={Logo} style={{ height: '50px', display: 'flex'}} />
+          </Box>  
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {PAGES.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0, fontFamily:'monospace',display: { xs: 'none', md: 'flex' }}}>
+            <Button variant="contained" color='warning'>Sign in</Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default NavBar;
